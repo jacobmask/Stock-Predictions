6b5360634f40eab0c198ecf2b1d6e916410384a0
Tim Campbell <campbtaf@uni.edu>
2021-03-08 14:43:07 -0600

    update requirements.txt and RNN.py

--- RNN.py
+++ RNN.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -6,7 +5,10 @@ import math
 import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas
+from keras.models import Sequential
+from keras.layers import Dense
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
 
 def main():
@@ -25,7 +27,10 @@ def main():
     min_max_scaler = preprocessing.MinMaxScaler()
     XScale = min_max_scaler.fit_transform(X)
 
-    print(XScale)
+    X_train, X_val_and_test, Y_train, Y_val_and_test = train_test_split(XScale, Y, test_size=0.3)
+    X_val, X_test, Y_val, Y_test = train_test_split(X_val_and_test, Y_val_and_test, test_size=0.5)
+
+    print(X_train.shape, X_val.shape, X_test.shape, Y_train.shape, Y_val.shape, Y_test.shape)
 
 
 if __name__ == '__main__':
