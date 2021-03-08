54f8799645832e2b2f8a2c0e82314e9d4ac08e33
Tim Campbell <campbtaf@uni.edu>
2021-03-08 14:17:37 -0600

    update RNN.py

--- RNN.py
+++ RNN.py
@@ -6,16 +6,26 @@ import math
 import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas
+from sklearn import preprocessing
 
 
 def main():
-    sin_wave = np.array([math.sin(x) for x in np.arange(200)])
-
-    plt.plot(sin_wave[:100])
-    plt.show()
+    # Graph modeling test:
+    # sin_wave = np.array([math.sin(x) for x in np.arange(200)])
+    # plt.plot(sin_wave[:100])
+    # plt.show()
 
     df = pandas.read_csv("Google.csv")
-    print(df)
+    dataset = df.values
+    #print(dataset)
+
+    X = dataset[:, 1:6]
+    Y = dataset[:, 2]
+
+    min_max_scaler = preprocessing.MinMaxScaler()
+    XScale = min_max_scaler.fit_transform(X)
+
+    print(XScale)
 
 
 if __name__ == '__main__':
