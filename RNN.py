c367a2e867c48d546e719c1cc8dedb55857269a1
Tim Campbell <campbtaf@uni.edu>
2021-03-22 13:50:43 -0500

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -10,34 +10,10 @@ import matplotlib.pyplot as plt
 import pandas
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
+from keras.layers import SimpleRNN
+from keras.layers import Dropout
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-
-
-# def main():
-# df = pandas.read_csv("Google.csv")
-# df.dropna()
-# dataset = df.values
-#
-# X = dataset[:, 1:6]
-# Y = dataset[:, 1]
-#
-# min_max_scaler = preprocessing.MinMaxScaler()
-# XScale = min_max_scaler.fit_transform(X)
-#
-# X_train, X_val_and_test, Y_train, Y_val_and_test = train_test_split(XScale, Y, test_size=0.3)
-# X_val, X_test, Y_val, Y_test = train_test_split(X_val_and_test, Y_val_and_test, test_size=0.5)
-# X_train = np.asarray(X_train).astype(np.float32)
-# Y_train = np.asarray(Y_train).astype(np.float32)
-# X_val = np.asarray(X_val).astype(np.float32)
-# Y_val = np.asarray(Y_val).astype(np.float32)
-#
-# # print(X_train.shape, X_val.shape, X_test.shape, Y_train.shape, Y_val.shape, Y_test.shape)
-#
-# model = Sequential([Dense(5, activation='relu', input_shape=(5,)), Dense(8, activation='relu'),
-#                     Dense(1, activation='sigmoid'), ])
-# model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
-# hist = model.fit(X_train, Y_train, batch_size=32, epochs=10, validation_data=(X_val, Y_val))
 
 
 def rnn():
@@ -48,8 +24,47 @@ def rnn():
     scaled_data = MinMaxScaler(feature_range=(0, 1))
     train_scaled = scaled_data.fit_transform(train)
     plt.plot(train_scaled)
-    plt.show()
 
+    X_train = []
+    Y_train = []
+
+    time_steps = 50
+
+    for i in range(time_steps, 1250):
+        X_train.append(train_scaled[i - time_steps:i, 0])
+        Y_train.append(train_scaled[i, 0])
+
+    X_train, Y_train = np.array(X_train), np.array(Y_train)
+
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+    # Initialize RNN:
+    regressor = Sequential()
+
+    # Adding the first RNN layer and some Dropout regularization
+    regressor.add(SimpleRNN(units=50, activation='tanh', return_sequences=True, input_shape=(X_train.shape[1], 1)))
+    regressor.add(Dropout(0.2))
+
+    # Adding the second RNN layer and some Dropout regularization
+    regressor.add(SimpleRNN(units=50, activation='tanh', return_sequences=True))
+    regressor.add(Dropout(0.2))
+
+    # Adding the third RNN layer and some Dropout regularization
+    regressor.add(SimpleRNN(units=50, activation='tanh', return_sequences=True))
+    regressor.add(Dropout(0.2))
+
+    # Adding the fourth RNN layer and some Dropout regularization
+    regressor.add(SimpleRNN(units=50))
+    regressor.add(Dropout(0.2))
+
+    # Adding the output layer
+    regressor.add(Dense(units=1))
+
+    # Compile the RNN
+    regressor.compile(optimizer='adam', loss='mean_squared_error')
+
+    # Fitting the RNN to the Training set
+    regressor.fit(X_train, Y_train, epochs=10, batch_size=32)
 
 
 if __name__ == '__main__':
