f33e39314466aac8ba26f23d01ded5713d8e8f30
Taleah Birkicht <taleahbirkicht@36-119.resnet-wifi.uni.edu>
2021-03-31 14:11:19 -0500

    Update data-clean

--- data-clean.py
+++ data-clean.py
@@ -7,16 +7,14 @@ Created on Wed Mar  3 14:15:47 2021
 """
 import yfinance as yf
 import pandas as pd
+from config import ticker_list
+import numpy as np
 
-stock_pop = ['NVDA', 'INTC', 'GOOGL', 'MSFT', 'AMZN', 
-             'FB', 'T', 'VZ', 'CSCO', 'ORCL']
+
 
 df1 = pd.DataFrame()
-current = []
 
-googl = yf.Ticker('GOOGL')
-for stock in stock_pop:
-    current.append(stock)
+for stock in ticker_list:
     #get ticker
     ticker = yf.Ticker(stock)
     #collect data
@@ -31,62 +29,47 @@ for stock in stock_pop:
     earn = earn.rename_axis(index='Date')
     earn.index = earn.index.astype(str) + '-12-31'
     earn.index = pd.to_datetime(earn.index)
+    rec = rec.drop(columns=['Firm', 'From Grade', 'Action'])
+    rec['To Grade'] = rec['To Grade'].str.lower().replace({'sell': 1.0,
+                                               'strong sell': 1.0,
+                                               'negative': 1.5,
+                                               'reduce': 1.5,
+                                               'underperform': 2.0,
+                                               'market underperform': 2.0,
+                                               'market underweight': 2.0,
+                                               'underweight': 2.0,
+                                               'sector underperform': 2.0,
+                                               'hold': 3.0,
+                                               'neutral': 3.0,
+                                               'market perform': 3.0,
+                                               'market weight': 3.0,
+                                               'perform': 3.0,
+                                               'equal-weight': 3.0,
+                                               'sector weight': 3.0,
+                                               'sector perform': 3.0,
+                                               'in-line': 3.0,
+                                               'average': 3.0,
+                                               'fair value': 3.0,
+                                               'peer perform': 3.0,
+                                               'mixed': 3.0,
+                                               'outperform': 4.0,
+                                               'market outperform': 4.0,
+                                               'overweight': 4.0,
+                                               'sector outperform': 4.0,
+                                               'positive': 4.5,
+                                               'strong buy': 5.0,
+                                               'buy': 5.0,
+                                               'long-term buy': 5.0,
+                                               '': np.nan})
+    rec = rec.dropna()
+    rec.index = rec.index.date
+    rec = rec.rename_axis(index='Date')
+    rec.to_csv('test.csv')
+    rec = rec.groupby('Date').agg({'To Grade': 'mean'})
     #combine
-    df = pd.concat([hist, bs, earn], axis=1)
+    df = pd.concat([hist, rec, bs, earn], axis=1)
     df = df.fillna(method='ffill')
     df = df.rename_axis(index='Date')
-    df.to_csv(stock + '.csv')
-    
-    
+    df.to_csv('./StockCSV/'+stock+'.csv')
 
-#df1.to_csv("test.csv")
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-''' 
-#collect data
-googl_hist = googl.history(period="max")
-googl_bs = googl.balance_sheet
-googl_earn = googl.earnings
-googl_rec = googl.recommendations
-
-#transform
-googl_bs = googl_bs.transpose()
-
-#googl_earn['Year'] = googl_earn['Year'].astype(str) + '-12-31'
-googl_hist.index = googl_hist.index.date
-#googl_hist = googl_hist[~googl_hist.index.duplicated(keep='first')]
-
-googl_bs.index = googl_bs.index.date
-#googl_bs = googl_bs[~googl_bs.index.duplicated(keep='first')]
-
-googl_earn = googl_earn.rename_axis(index='Date')
-googl_earn.index = googl_earn.index.astype(str) + '-12-31'
-googl_earn.index = pd.to_datetime(googl_earn.index)
-print(googl_earn)
-
-#print(googl_rec)
-
-
-df = pd.concat([googl_hist, googl_bs, googl_earn], axis=1)
-df = df.fillna(method='ffill')
-df = df.rename_axis(index='Date')
-print(df)
-df.to_csv("Google.csv")
-'''
+ 
