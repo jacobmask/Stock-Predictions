b58e9b165a913a19bb126a36e584a2c7ef93179e
root <root@e3-1230v2.BL-phx0.1.141.8.6.F2.securedservers.com>
2021-04-07 09:13:59 -0400

    Updated StockCSV's and new automation outputs

--- automation/runRNNScript.sh
+++ automation/runRNNScript.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/pnap32032/automation
 make runRNN > /home/pnap32032/automation/rnnLogs/"$(date +"%Y_%m_%d_%I_%M_%p").txt"
+mailx -s "RNN Ran" jacobmask@yahoo.com < /home/pnap32032/automation/newDayLogs.txt
