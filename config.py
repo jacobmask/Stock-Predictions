532eefc7d6b86cbf74713de9cbd3c652c3cc9bbb
Tim Campbell <campbtaf@uni.edu>
2021-04-09 14:10:50 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 ticker_list = ['NVDA','INTC','GOOGL','MSFT','AMZN','FB','T','VZ','CSCO','ORCL']
 
 #Epoch is the amount of times our RNN trains with the data used in RNN.py
-epoch = 10
+cols = [1, 2, 3, 4, 5, 6, 7]
 
