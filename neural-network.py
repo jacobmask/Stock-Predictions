af9e396d605554ed16132d82dd1c61f645523bb8
Tim Campbell <campbtaf@uni.edu>
2021-04-21 14:43:21 -0500

    Update neural-network.py

--- neural-network.py
+++ neural-network.py
@@ -1,3 +1,9 @@
+"""
+Author: Tim Campbell
+Modified: 3/24/2021
+Notes: Implemented config ticker list.
+"""
+
 import os
 from os import listdir
 from os.path import isfile, join
@@ -15,7 +21,6 @@ def main():
     os.chdir('StockCSVRecs')
     for csv_name in csv_files:
         df = pd.read_csv(csv_name, usecols=cols)
-        # date = pandas.read_csv(csv_name, usecols=[0])
         stock_name = "$" + csv_name[:-8]
         train_data = df.copy()
 
@@ -50,15 +55,19 @@ def neural_network(stock_name, train_data, output_data):
 
     # calculate num of epochs based on length of dataset
     weighted_epoch = round(len(train_data)/100)
+
     print("-----Beginning training for %s-----" % stock_name)
-    stock_model.fit(train_data, output_data, epochs=weighted_epoch)
+
+    stock_model.fit(train_data, output_data, epochs=weighted_epoch, verbose=0)
 
     prediction = stock_model.predict(train_data, batch_size=None, verbose=0, steps=1, callbacks=None,
                                      max_queue_size=10, workers=1, use_multiprocessing=False)
 
+    prediction_price = float(str(prediction[-1]).strip("[").strip("]"))
+
     print()
-    print("Same day price prediction for %s: " % stock_name, prediction[-1])
-    print("Actual price for %s: $" % stock_name + str(round(output_data["Close"][output_data.index[-1]],2)))
+    print("Next closing price prediction for %s:" % stock_name, "$" + str(round(prediction_price, 2)))
+    print("Current price for %s: $" % stock_name + str(round(output_data["Close"][output_data.index[-1]], 2)))
     print()
 
     plt.plot(prediction)
