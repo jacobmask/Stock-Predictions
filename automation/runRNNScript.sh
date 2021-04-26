54bef78190f90e29c8ba93a7bcaf2c9dc9a7af91
root <root@e3-1230v2.BL-phx0.1.141.8.6.F2.securedservers.com>
2021-04-26 18:06:13 -0400

    Edit to makefile

--- automation/runRNNScript.sh
+++ automation/runRNNScript.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /home/pnap32032/automation
-make runRNN > /home/pnap32032/automation/rnnLogs/"$(date +"%Y_%m_%d_%I_%M_%p").txt"
-mailx -s "RNN Ran" jacobmask@yahoo.com < /home/pnap32032/automation/rnnLogs/"$(date +"%Y_%m_%d_%I_%M_%p").txt"
+make runRNN > rnnLogs/"$(date +"%Y_%m_%d_%I_%M_%p").txt"
+mailx -s "RNN Ran" jacobmask@yahoo.com < rnnLogs/"$(date +"%Y_%m_%d_%I_%M_%p").txt"
