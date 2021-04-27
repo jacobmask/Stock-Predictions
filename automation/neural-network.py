121fc0441672bf828213b477c6301ce47ab8b873
root <root@e3-1230v2.BL-phx0.1.141.8.6.F2.securedservers.com>
2021-04-26 20:18:07 -0400

    Updated automation NN

--- automation/neural-network.py
+++ automation/neural-network.py
@@ -1,9 +1,8 @@
 """
 Author: Tim Campbell
 Modified: 3/24/2021
-Notes: Implements a sequential neural network using keras.
+Notes: Implemented config ticker list.
 """
-
 
 import os
 from os import listdir
@@ -16,7 +15,6 @@ from config import cols
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-np.set_printoptions(precision=3, suppress=True)
 
 
 def main():
@@ -26,12 +24,12 @@ def main():
         df = pd.read_csv(csv_name, usecols=cols)
         # date = pandas.read_csv(csv_name, usecols=[0])
         stock_name = "$" + csv_name[:-8]
-        #train_data = df.copy()
+        # train_data = df.copy()
 
         # Single column output
-        #output_data = pd.DataFrame({"Close": df.pop("Close")})
+        # output_data = pd.DataFrame({"Close": df.pop("Close")})
+        tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
         neural_network(stock_name, df)
-
 
         # Double column output
         # output_data = pandas.DataFrame()
@@ -41,23 +39,21 @@ def main():
 
 
 def neural_network(stock_name, df):
+    # normalize = tf.keras.layers.experimental.preprocessing.Normalization()
 
-    #normalize = tf.keras.layers.experimental.preprocessing.Normalization()
-    
     X = df.drop('Close', axis=1)
     y = np.ravel(df['Close'])
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, shuffle=False)
     scaler = MinMaxScaler().fit(X_train)
     X_train = scaler.transform(X_train)
     X_test = scaler.transform(X_test)
-    
 
-    #train_data = np.array(train_data)
+    # train_data = np.array(train_data)
 
-    #normalize.adapt(train_data)
+    # normalize.adapt(train_data)
 
     stock_model = tf.keras.Sequential([
-        #normalize,
+        # normalize,
         layers.Dense(32),
         layers.Dense(64),
         layers.Dense(1)])
@@ -67,26 +63,24 @@ def neural_network(stock_name, df):
                         metrics=tf.keras.metrics.Accuracy())
 
     # calculate num of epochs based on length of dataset
-    weighted_epoch = round(len(df)/100)
+    weighted_epoch = round(len(df) / 100)
     print("-----Beginning training for %s-----" % stock_name)
 
     stock_model.fit(X_train, y_train, epochs=weighted_epoch, verbose=0)
 
     prediction = stock_model.predict(X_test, batch_size=None, verbose=0, steps=1, callbacks=None,
                                      max_queue_size=10, workers=1, use_multiprocessing=False)
-    
+
     prediction_price = float(str(prediction[-1]).strip("[").strip("]"))
 
-
-    
     print()
-    print("Next day price prediction for %s: " % stock_name, (str(round(prediction_price, 2))))
-    print("Actual price for %s: $" % stock_name + str(round(y_test[-1], 2)))
+    print("Next market close price prediction for %s: $" % stock_name, (str(round(prediction_price, 2))))
+    print("Current price for %s: $" % stock_name + str(round(y_test[-1], 2)))
     print()
 
     plt.plot(prediction, label='Predicted')
     plt.plot(y_test, label='Actual')
-    # plt.plot(output_data)
+    #plt.plot(output_data)
     plt.title(stock_name)
     plt.xlabel("Date")
     plt.ylabel("Price")
@@ -94,7 +88,6 @@ def neural_network(stock_name, df):
     plt.show()
 
 
-
-
 if __name__ == '__main__':
     main()
+
