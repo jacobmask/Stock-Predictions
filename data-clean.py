b18de7f7ad864ae32be05dd0c493bf8868628b53
Taleah Birkicht <taleahbirkicht@252-241.wireless.uni.edu>
2021-04-28 14:27:57 -0500

    Updated data-clean.py to clean up code

--- data-clean.py
+++ data-clean.py
@@ -1,41 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar  3 14:15:47 2021
-
-@author: taleahbirkicht
-
-Modified: 3/24/2021
-Author: Jacob Mask
-Notes: Implemented config ticker list.
+Authors: Taleah Birkicht, Tim Campbell, Jacob Mask
+Last Modified: 4/28/21
+Description: This file automatically adds or modifies a csv for each of the
+stock population determined by the config file into a folder called StockCSVRecs.
+The csv will contain historical market data for the stock price such as
+High Low, Close, Open as well as analyst recommendation data. The analyst
+recommendation data is one column named "Recommendation".
 """
 import yfinance as yf
 import pandas as pd
 from config import ticker_list
 import numpy as np
-import datetime as dt
+import os
 
-
-df1 = pd.DataFrame()
 
 for stock in ticker_list:
     #get ticker
     ticker = yf.Ticker(stock)
     #collect data
     hist = ticker.history(period="max")
-    #bs = ticker.balance_sheet
-    #earn = ticker.earnings
     rec = ticker.recommendations
     #transform
-    #bs = bs.transpose()
     hist.index = hist.index.date
     hist = hist.rename_axis(index='Date')
-
-    #bs.index = bs.index.date
-    #earn = earn.rename_axis(index='Date')
-    #earn.index = earn.index.astype(str) + '-12-31'
-    #earn.index = pd.to_datetime(earn.index)
     rec = rec.drop(columns=['Firm', 'From Grade', 'Action'])
+    #applies quantitative value to categorical variables
     rec['To Grade'] = rec['To Grade'].str.lower().replace({'sell': 1.0,
                                                'strong sell': 1.0,
                                                'negative': 1.5,
@@ -71,17 +62,24 @@ for stock in ticker_list:
     rec = rec.dropna()
     rec.index = rec.index.date
     rec = rec.rename_axis(index='Date')
+    #averages the recommendations for a given day
     rec = rec.groupby('Date').agg({'To Grade': 'mean'})
+    rec = rec.rename(columns={'To Grade': 'Recommendation'})
     hist = hist.rename_axis(index='Date')
-
+    
+    #only obtain dates larger than first analyst recommendation
     hist2 = hist[(hist.index >= rec.index[0])]
 
-    #combine
+    #combine market data and analyst recommendations
     df = pd.concat([hist2, rec], axis=1, join='outer')
+    #analyst recommendations are forward filled in until the next recommendation
+    #is made
     df = df.fillna(method='ffill')
     df = df.rename_axis(index='Date')
-    #df = df.append({'Date': dt.datetime.now() + dt.timedelta(days=1)}, ignore_index=True)
-    hist = hist.rename_axis(index='Date')
-    #hist.to_csv('./StockCSV/'+stock+'.csv')
     
+    #creates new folder if it does not already exist
+    if not os.path.exists('StockCSVRecs'):
+        os.makedirs('StockCSVRecs')
+    
+    #writes to csv file
     df.to_csv('./StockCSVRecs/'+stock+'_rec'+'.csv')
