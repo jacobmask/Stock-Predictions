f6a1c6526b614a5014574e5526f46cbae5e4eec5
root <root@e3-1230v2.BL-phx0.1.141.8.6.F2.securedservers.com>
2021-04-29 21:31:13 -0400

    Updated automation scripts for new user use

--- automation/newDayScript.sh
+++ automation/newDayScript.sh
@@ -3,11 +3,11 @@
 #Simple bash script to run our new Day stock puller
 
 
-#Change this to your directory your Makefile is in for use with crontabs
+#Change this to your directory your Makefile is in for use with crontabs and uncomment it
 #Example: /home/user/4620-Predicting-Stock-Prices/automation 
-cd /home/pnap32032/automation
+#cd /home/pnap32032/automation
 make newDay
 
 #Change this output directory to your logs
 #Example: /home/user/4620-Predicting-Stock-Prices/automation/logs/newDayLogs.txt
-echo "$(date +"%Y_%m_%d_%I_%M_%p")" >> /home/pnap32032/automation/logs/newDayLogs.txt
+echo "$(date +"%Y_%m_%d_%I_%M_%p")" >> logs/newDayLogs.txt
