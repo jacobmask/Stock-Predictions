c4a6795954362a6bda3991e4896eb1dbf0be26a5
root <root@e3-1230v2.BL-phx0.1.141.8.6.F2.securedservers.com>
2021-04-29 21:23:26 -0400

    Added Comments to automation scripts

--- automation/newDayScript.sh
+++ automation/newDayScript.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+#Author: Jacob Mask
+#Simple bash script to run our new Day stock puller
+
+
+#Change this to your directory your Makefile is in for use with crontabs
+#Example: /home/user/4620-Predicting-Stock-Prices/automation 
+cd /home/pnap32032/automation
 make newDay
-echo "$(date +"%Y_%m_%d_%I_%M_%p")" >> logs/newDayLogs.txt
+
+#Change this output directory to your logs
+#Example: /home/user/4620-Predicting-Stock-Prices/automation/logs/newDayLogs.txt
+echo "$(date +"%Y_%m_%d_%I_%M_%p")" >> /home/pnap32032/automation/logs/newDayLogs.txt
--- automation/runNNScript.sh
+++ automation/runNNScript.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-make runNN > logs/neural-network-logs/"$(date +"%Y_%m_%d_%I_%M_%p").txt"
-mailx -s "NN Ran" jacobmask@yahoo.com < logs/neural-network-logs/"$(date +"%Y_%m_%d_%I_%M_%p").txt"
+#Author: Jacob Mask
+#Simple bash script to run our Neural Network
+
+#Change this to your directory your Makefile is in for us with crontabs
+#Example: /home/user/4620-Predicting-Stock-Prices/automation
+cd /home/pnap32032/automation 
+
+#Change this output directory to your logs folder for neural-network outputs
+#Example: /home/user/4620-Predicting-Stock-Prices/automation/logs/neural-network-logs/
+make runNN > /home/pnap32032/automation/logs/neural-network-logs/"$(date +"%Y_%m_%d_%I_%M_%p").txt" 
+
+#Similar to the example before, please change the email to something else, or add your own email to get updates on ticker predictions
+mailx -s "NN Ran" jacobmask@yahoo.com < /home/pnap32032/automation/logs/neural-network-logs/"$(date +"%Y_%m_%d_%I_%M_%p").txt"
